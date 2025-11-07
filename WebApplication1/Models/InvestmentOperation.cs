63d4d67d9daebccbdb963371eb9bf25ae2fa8f3a
Daniil465346 <dkuzminov@sfedu.ru>
2025-11-07 15:33:47 +0300

    Add files via upload

---
+++ WebApplication1/Models/InvestmentOperation.cs
@@ -0,0 +1,15 @@
+﻿namespace InvestmentApi.Models
+{
+    public class InvestmentOperation
+    {
+        public int Id { get; set; }
+        public int SecurityId { get; set; } 
+        public int Quantity { get; set; }   
+        public decimal PurchasePricePerShare { get; set; } 
+        public decimal Commission { get; set; } 
+        public decimal? TargetBuyPrice { get; set; } 
+
+        
+        public decimal TotalCost => (Quantity * PurchasePricePerShare) + Commission;
+    }
+}
---
+++ WebApplication1/Models/Security.cs
@@ -0,0 +1,10 @@
+﻿namespace InvestmentApi.Models
+{
+    public class Security
+    {
+        public int Id { get; set; }
+        public string Ticker { get; set; } 
+        public string Name { get; set; }   
+        public decimal CurrentPrice { get; set; } 
+    }
+}
